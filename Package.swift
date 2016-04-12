f75671bb5e7193b7c7c7dfa1194db331e059c08a
Yuki Takei <yuki@miketokyo.com>
2016-04-13 05:16:24 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "SuvTestApp",
 	dependencies: [
-    .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 1)
+    .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 2)
   ]
 )
